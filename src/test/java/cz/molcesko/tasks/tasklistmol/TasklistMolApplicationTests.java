4406983dfc11fca4a99786b1245dd96f3bf4ceb4
nlebn001 <nlebn001@gmail.com>
2021-02-01 12:56:33 +0100

    Initial commit

---
+++ src/test/java/cz/molcesko/tasks/tasklistmol/TasklistMolApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.molcesko.tasks.tasklistmol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistMolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
